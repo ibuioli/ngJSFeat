d5c276aa57701c714c53eb1b1d1ce0eddba990fa
Ignacio Buioli <ibuioli@gmail.com>
2021-02-05 02:34:38 -0300

    feat: update to Ng11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,21 @@ import * as jsfeat from 'jsfeat';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @ViewChild('hardwareVideo') hardwareVideo: any;
+export class AppComponent implements OnInit {
+  @ViewChild('hardwareVideo', { static: true }) hardwareVideo: any;
   constraints: any;
   canvas: any;
   context: any;
   img_u8: any;
 
-  constructor(){}
+  constructor() {}
 
-  ngOnInit(){
+  ngOnInit(): any {
     this.constraints = {
       audio: false,
       video: {
-        width: {ideal:640},
-        height: {ideal:280}
+        width: {ideal: 640},
+        height: {ideal: 480}
       }
     };
 
@@ -29,19 +29,19 @@ export class AppComponent {
     this.videoStart();
   }
 
-  videoStart(){
-    let video = this.hardwareVideo.nativeElement;
-    let n = <any>navigator;
+  public videoStart(): void {
+    const video = this.hardwareVideo.nativeElement;
+    const n = <any>navigator;
 
     n.getUserMedia = ( n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia  || n.msGetUserMedia );
 
-    n.mediaDevices.getUserMedia(this.constraints).then(function(stream) {
-      if ("srcObject" in video) {
+    n.mediaDevices.getUserMedia(this.constraints).then((stream: any) => {
+      if ('srcObject' in video) {
         video.srcObject = stream;
       } else {
         video.src = window.URL.createObjectURL(stream);
       }
-      video.onloadedmetadata = function(e) {
+      video.onloadedmetadata = (e: any) => {
         video.play();
       };
     });
@@ -52,19 +52,19 @@ export class AppComponent {
     this.loop();
   }
 
-  loop = () =>{
+  loop = () => {
     this.context.drawImage(this.hardwareVideo.nativeElement, 0, 0, this.canvas.width, this.canvas.height);
-    var imageData = this.context.getImageData(0, 0, 640, 480);
+    const imageData = this.context.getImageData(0, 0, 640, 480);
 
     jsfeat.imgproc.grayscale(imageData.data, 640, 480, this.img_u8);
 
-    var r = 10|0;
-    var kernel_size = (r+1) << 1;
+    const r = 10 | 0;
+    const kernel_size = (r+1) << 1;
     jsfeat.imgproc.gaussian_blur(this.img_u8, this.img_u8, kernel_size, 0);
 
-    var data_u32 = new Uint32Array(imageData.data.buffer);
-    var alpha = (0xff << 24);
-    var i = this.img_u8.cols*this.img_u8.rows, pix = 0;
+    const data_u32 = new Uint32Array(imageData.data.buffer);
+    const alpha = (0xff << 24);
+    let i = this.img_u8.cols * this.img_u8.rows, pix = 0;
     while(--i >= 0) {
         pix = this.img_u8.data[i];
         data_u32[i] = alpha | (pix << 16) | (pix << 8) | pix;
